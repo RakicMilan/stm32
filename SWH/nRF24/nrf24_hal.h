d7ab0fba672d2f15433253f41620fe44b2437651
Milan Rakic <milanrakic1988@gmail.com>
2018-10-01 20:41:32 +0200

    Changed GPIOs for nRF24l01 module

--- SWH/nRF24/nrf24_hal.h
+++ SWH/nRF24/nrf24_hal.h
@@ -1,17 +1,14 @@
 #ifndef __NRF24_HAL_H
 #define __NRF24_HAL_H
 
-
 // Hardware abstraction layer for NRF24L01+ transceiver (hardware depended functions)
 // GPIO pins definition
 // GPIO pins initialization and control functions
 // SPI transmit functions
 
-
 // Peripheral libraries
 #include <stm32f10x_gpio.h>
 #include <stm32f10x_spi.h>
-
 
 // SPI port peripheral
 #define nRF24_SPI_PORT             SPI2
@@ -21,7 +18,7 @@
 
 // CE (chip enable) pin (PB11)
 #define nRF24_CE_PORT              GPIOB
-#define nRF24_CE_PIN               GPIO_Pin_11
+#define nRF24_CE_PIN               GPIO_Pin_9
 #define nRF24_CE_L()               GPIO_ResetBits(nRF24_CE_PORT, nRF24_CE_PIN)
 #define nRF24_CE_H()               GPIO_SetBits(nRF24_CE_PORT, nRF24_CE_PIN)
 
@@ -33,8 +30,7 @@
 
 // IRQ pin (PB10)
 #define nRF24_IRQ_PORT             GPIOB
-#define nRF24_IRQ_PIN              GPIO_Pin_10
-
+#define nRF24_IRQ_PIN              GPIO_Pin_8
 
 // Function prototypes
 void nRF24_GPIO_Init(void);
